db95d9147f4282c9067515b088ba304d9ab6ced5
adrian <cognominative@gmail.com>
2020-12-07 10:51:30 -0500

    Add validators and tests for day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,8 +1,12 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::collections::HashSet;
 
+use regex::Regex;
 use parse_display::{Display, FromStr};
 
 #[derive(Display, FromStr, PartialEq, Debug)]
@@ -10,6 +14,49 @@ use parse_display::{Display, FromStr};
 struct PassportField {
     key: String,
     val: String
+}
+
+impl PassportField {
+    fn is_valid(&self) -> bool {
+        lazy_static! {
+            static ref HGT_RE: Regex = Regex::new(r"^(\d+)(cm|in)$").unwrap();
+            static ref HCL_RE: Regex = Regex::new(r"^#[a-f0-9]{6}$").unwrap();
+            static ref PID_RE: Regex = Regex::new(r"^\d{9}$").unwrap();
+        }
+        match self.key.as_str() {
+            "byr" => {
+                let year: u32 = self.val.parse().unwrap();
+                (1920 <= year) & (year <= 2002)
+            },
+            "iyr" => {
+                let year: u32 = self.val.parse().unwrap();
+                (2010 <= year) & (year <= 2020)
+            },
+            "eyr" => {
+                let year: u32 = self.val.parse().unwrap();
+                (2020 <= year) & (year <= 2030)
+            },
+            "hgt" => {
+                if let Some(caps) = HGT_RE.captures(self.val.as_str()) {
+                    let value = caps.get(1).map_or(0, |m| m.as_str().parse::<u32>().unwrap());
+                    let unit = caps.get(2).map_or("", |m| m.as_str());
+                    match unit {
+                        "cm" => (150 <= value) & (value <= 193),
+                        "in" => (59 <= value) & (value <= 76),
+                        _ => false
+                    }
+                }
+                else {
+                    false
+                }
+            },
+            "hcl" => HCL_RE.is_match(self.val.as_str()),
+            "ecl" => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+                        .contains(&self.val.as_str()),
+            "pid" => PID_RE.is_match(self.val.as_str()),
+            _ => true
+        }
+    }
 }
 
 
@@ -50,4 +97,77 @@ fn main() {
         .count();
 
     println!("Part 1: {}", num_complete)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_validate_byr() {
+        let field = PassportField {key: "byr".to_string(),
+                                   val: "2002".to_string()};
+        assert!(field.is_valid());
+
+        let field = PassportField {key: "byr".to_string(),
+                                   val: "2010".to_string()};
+        assert!(!field.is_valid());
+    }
+
+    #[test]
+    fn test_validate_hgt() {
+        let field = PassportField {key: "hgt".to_string(),
+                                   val: "60in".to_string()};
+        assert!(field.is_valid());
+
+        let field = PassportField {key: "hgt".to_string(),
+                                   val: "190cm".to_string()};
+        assert!(field.is_valid());
+
+        let field = PassportField {key: "hgt".to_string(),
+                                   val: "190in".to_string()};
+        assert!(!field.is_valid());
+
+        let field = PassportField {key: "hgt".to_string(),
+                                   val: "60".to_string()};
+        assert!(!field.is_valid());
+    }
+
+    #[test]
+    fn test_validate_ecl() {
+        let field = PassportField {key: "ecl".to_string(),
+                                   val: "brn".to_string()};
+        assert!(field.is_valid());
+
+        let field = PassportField {key: "ecl".to_string(),
+                                   val: "wat".to_string()};
+        assert!(!field.is_valid());
+    }
+
+    #[test]
+    fn test_validate_hcl() {
+        let field = PassportField {key: "hcl".to_string(),
+                                   val: "#123abc".to_string()};
+        assert!(field.is_valid());
+
+        let field = PassportField {key: "hcl".to_string(),
+                                   val: "#123abz".to_string()};
+        assert!(!field.is_valid());
+
+        let field = PassportField {key: "hcl".to_string(),
+                                   val: "123abc".to_string()};
+        assert!(!field.is_valid());
+    }
+
+    #[test]
+    fn test_validate_pid() {
+        let field = PassportField {key: "pid".to_string(),
+                                   val: "000000001".to_string()};
+        assert!(field.is_valid());
+
+        let field = PassportField {key: "pid".to_string(),
+                                   val: "0123456789".to_string()};
+        assert!(!field.is_valid());
+    }
 }
