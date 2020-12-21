bdc600bf0d8353dfe30a8e08dd75ab5ad659b325
adrian <cognominative@gmail.com>
2020-12-20 20:48:44 -0500

    Day 12 Part 2, now with rotation matrix

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -15,7 +15,27 @@ fn simple_update(state: (i32, i32, usize), step: &String) -> (i32, i32, usize) {
         'W' => (-dist, 0, 0),
         'R' => (0, 0, (dist / 90) as usize),
         'L' => (0, 0, 360 - (dist / 90) as usize),
-        _ => state
+        _ => (0, 0, 0)
+    }
+}
+
+fn part2_update(state: (i32, i32, i32, i32), step: &String) -> (i32, i32, i32, i32) {
+    let (mut dir, mut dist) = (step.chars().nth(0).unwrap(),
+        i32::from_str(step.get(1..).unwrap()).unwrap());
+
+    if dir == 'R' {dir = 'L'; dist = 360 - dist;};
+
+    match dir {
+        'N' => (0, 0, 0, dist),
+        'S' => (0, 0, 0, -dist),
+        'E' => (0, 0, dist, 0),
+        'W' => (0, 0, -dist, 0),
+        'L' => {
+            let rot = [(1, 0), (0, 1), (-1, 0), (0, -1)][(dist / 90) as usize];
+            (0, 0, (rot.0 - 1)* state.2 - rot.1 * state.3, rot.1 * state.2 + (rot.0 - 1) * state.3)
+        },
+        'F' => (dist * state.2, dist * state.3, 0, 0),
+        _ => (0, 0, 0, 0),
     }
 }
 
@@ -30,4 +50,11 @@ fn main() {
     });
 
     println!("Part 1: {:?}", position.0.abs() + position.1.abs());
+
+    let position = steps.iter().fold((0, 0, 10, 1), |state, step| {
+        let update = part2_update(state, step);
+        (state.0 + update.0, state.1 + update.1, state.2 + update.2, state.3 + update.3)
+    });
+
+    println!("Part 2: {:?}", position.0.abs() + position.1.abs());
 }
